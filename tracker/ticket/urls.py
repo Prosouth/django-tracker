d634c59aacf7ebeae5c2ff15d5b0e8c6ce0e8180
Prosouth <info@sebastiensaez.ch>
2015-07-15 20:12:28 +0200

    updated views and urls

--- tracker/ticket/urls.py
+++ tracker/ticket/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from views import home, ticket_listing
+from views import home, ticket_listing, ticket_list, ticket_detail
 
 urlpatterns = [
     url(r'^home/$', home, name="home"),
     url(r'^home/(\w+)$', home, name="home"),
-    url(r'^$', ticket_listing, name="listing")
+    url(r'^$', ticket_listing, name="listing"),
+    url(r'^list/(\w+)', ticket_list, name="list"),
+    url(r'^detail/(\d+)', ticket_detail, name="detail")
 ]
--- tracker/ticket/views.py
+++ tracker/ticket/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from datetime import date, timedelta
 from models import Task
 
 
@@ -14,3 +15,20 @@ def home(request, name=None):
 def ticket_listing(request):
     objets = Task.objects.all().order_by('-due_date')
     return render_to_response('ticket/list.html', {'objets': objets})
+
+
+def ticket_detail(request, id):
+    objet = Task.objects.get(pk=id)
+    return render_to_response('ticket/detail.html', {'objet': objet})
+
+
+def ticket_list(request, filter):
+    if filter == 'closed':
+        objets = Task.objects.filter(closed=True)
+    elif filter == 'open':
+        objets = Task.objects.filter(closed=False)
+    elif filter == 'week':
+        objets = Task.objects.filter(
+            due_date__lte=date.today() + timedelta(days=7)).filter(
+                due_date__gte=date.today())
+        return render_to_response('ticket/list.html', {'objets': objets})
